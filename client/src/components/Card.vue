1c69249d9b9b796d443d59574e0f0ea25f9f06b8
Vadim Sorokin <k500i90@gmail.com>
2020-05-21 18:58:27 +0300

    Play video on card mouseenter

--- client/src/components/Card.vue
+++ client/src/components/Card.vue
@@ -1,7 +1,10 @@
 <template lang="jade">
   .card-wrap
-    .card(v-perspective)
-      .image-wrap: .image(:style="bg")
+    .card(v-perspective :class="{mouseentered: mouseentered}" @mouseenter="mouseenter" @mouseleave="mouseleave")
+      .image-wrap
+        video(preload ref="video" loop)
+          source(:src="video")
+        .image(:style="bg")
       .shadow-wrap: .shadow(:style="bg")
 </template>
 
@@ -15,12 +18,34 @@ export default {
       required: true
     }
   },
+  
+  data() {
+    return {
+      mouseentered: false
+    }
+  },
 
   computed: {
     bg() {
       const coverURL = this.$APIRequest.getURL(`file/${this.data.cover_id}`);
       
       return {'background-image': `url(${coverURL})`}
+    },
+    
+    video() {
+      return this.$APIRequest.getURL(`file/${this.data.video_id}`);
+    }
+  },
+  
+  methods: {
+    mouseenter() {
+      this.mouseentered = true;
+      this.$refs.video.play();
+    },
+    
+    mouseleave() {
+      this.mouseentered = false;
+      this.$refs.video.pause();
     }
   }
 }
